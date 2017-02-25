ec961af16c9c854c71393d01e9fceca0fc8cd4ed
Brigette Eckert <Brigette.Eckert@live.com>
2017-02-25 13:36:01 -0800

    added brew model

---
+++ src/app/brew.model.ts
@@ -0,0 +1,9 @@
+export class Brew {
+  constructor(
+    public name: string,
+    public brand: string,
+    public price: num,
+    public alcoholContent: num,
+    public pintsRemaining: num,
+  ){}
+}
